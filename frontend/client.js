d23adeb188eaf7288f32223c60480145257029d5
Johan Ehinger <johan.ehinger@hotmail.com>
2021-12-16 14:02:41 +0100

    Step 3: Implementing user input validation for sign-in and sign-up forms

--- frontend/client.js
+++ frontend/client.js
@@ -6,3 +6,29 @@ displayview = function () {
 window.onload = function () {
   displayview();
 };
+
+function loginValidation() {
+  password = document.getElementById("password").value;
+  if (password.length < 5) {
+    document.getElementById("login-error").innerHTML =
+      "Password must be longer than 5 characters!";
+    return false;
+  }
+  return true;
+}
+
+function signUpValidation() {
+  password = document.getElementById("password-sign-up").value;
+  repeat_password = document.getElementById("repeat-password-sign-up").value;
+  if (password.length < 5 || repeat_password.length < 5) {
+    document.getElementById("sign-up-error").innerHTML =
+      "Password must be longer than 5 characters!";
+    return false;
+  }
+  if (password != repeat_password) {
+    document.getElementById("sign-up-error").innerHTML =
+      "Passwords must match!";
+    return false;
+  }
+  return true;
+}
