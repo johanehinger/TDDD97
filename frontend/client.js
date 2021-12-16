28ca7df64c2ecd7ae806bacfdb098ebcf5e488b7
Johan Ehinger <johan.ehinger@hotmail.com>
2021-12-16 13:03:02 +0100

    Step 2: Displaying a certain view

--- frontend/client.js
+++ frontend/client.js
@@ -1,1 +1,8 @@
+displayview = function () {
+  document.getElementById("view").innerHTML =
+    document.getElementById("welcome-view").innerHTML;
+};
 
+window.onload = function () {
+  displayview();
+};
