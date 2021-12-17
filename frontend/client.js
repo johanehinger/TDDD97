b3c8531e578e20e51dbdf8da3f7dd72e860e43a6
Johan Ehinger <johan.ehinger@hotmail.com>
2021-12-17 07:12:07 +0100

    Step 6: Implementing tabs

--- frontend/client.js
+++ frontend/client.js
@@ -71,3 +71,17 @@ function signUp() {
   displayview();
   return response.success;
 }
+
+function navigate(evt, tab) {
+  var i, tabcontent, tablinks;
+  tabcontent = document.getElementsByClassName("tabcontent");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
+  tablinks = document.getElementsByClassName("tablinks");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" active", "");
+  }
+  document.getElementById(tab).style.display = "block";
+  evt.currentTarget.className += " active";
+}
