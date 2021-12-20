c033f7d627581a2d49b846850b187b2b1fd7dd7b
Johan Ehinger <johan.ehinger@hotmail.com>
2021-12-20 12:11:10 +0100

    Step 1: starting with SQLite

--- backend/server.py
+++ backend/server.py
@@ -1,7 +1,27 @@
-from flask import Flask
+from flask import Flask, request
+import math
+import random
 
 app = Flask(__name__)
+
+def generateToken():
+    letters = "abcdefghiklmnopqrstuvwwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890"
+    token = ""
+    for i in range(0, 36):
+        token += letters[math.floor(random.random() * len(letters))]
+    return token
+    
 
 @app.route("/")
 def hello_world():
     return "<p>Hello, World!</p>"
+
+@app.route("/sign_in", methods=['POST'])
+def sign_in():
+    email = request.headers.get("email")
+    password = request.headers.get("password")
+    token = generateToken()
+    return {"success": True, "message": "Successfully signed in.", "data": token }
+
+
+    
